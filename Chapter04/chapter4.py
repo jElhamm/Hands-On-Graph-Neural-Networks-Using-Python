bff6bbd0fbf0a408048e004b4984a1b42cedeea1
jelhamm <elhamjafari200312@gmail.com>
2024-08-14 21:27:12 +0430

    Add project overview, dependencies, and initial imports for GNN tasks

---
+++ Chapter04/chapter4.py
@@ -0,0 +1,43 @@
+# ***************************************************************************************************************************
+#                                                                                                                           *
+#                               Hands On Graph Neural Networks Using Python  -  CHAPTER 4                                   *
+#                                                                                                                           *
+#       This script covers various tasks related to graph processing and recommendation systems, including:                 *
+#                                                                                                                           *
+#       1. Graph Creation and Visualization:                                                                                *
+#           - Creates a random graph using the Erdos-Renyi model.                                                           *
+#           - Visualizes the graph using network plotting tools.                                                            *
+#                                                                                                                           *
+#       2. Random Walk and Node2Vec:                                                                                        *
+#           - Performs random walks on a graph to generate node sequences.                                                  *
+#           - Applies `Node2Vec` to learn node embeddings from these sequences.                                             *
+#           - Trains a `Word2Ve`c model on the generated walks.                                                             *
+#           - Evaluates the quality of the learned embeddings using a Random Forest classifier.                             *
+#                                                                                                                           *
+#       3. MovieLens Data Processing and Recommendation:                                                                    *
+#           - Downloads and extracts MovieLens dataset.                                                                     *
+#           - Constructs a movie co-occurrence graph from user ratings.                                                     *
+#           - Uses Node2Vec to learn embeddings for movies and recommends similar movies based on these embeddings.         *
+#                                                                                                                           *
+#       The script integrates graph analysis techniques with practical applications in recommendation systems.              *
+#                                                                                                                           *
+# ***************************************************************************************************************************
+
+
+
+# !pip install -q node2vec==0.4.6
+# !pip install -qI gensim==4.3.0
+import random
+import numpy as np
+import pandas as pd
+import networkx as nx
+from io import BytesIO
+from zipfile import ZipFile
+from node2vec import Node2Vec
+import matplotlib.pyplot as plt
+from urllib.request import urlopen
+from collections import defaultdict
+from sklearn.metrics import accuracy_score
+from gensim.models.word2vec import Word2Vec
+from sklearn.ensemble import RandomForestClassifier
+    
