09d84507dc1321dc596e06f7c6b0f17732334a68
jelhamm <elhamjafari200312@gmail.com>
2024-08-19 22:13:27 +0430

    Add training loop for VanillaGNN with accuracy and loss tracking

--- Chapter05/chapter5.py
+++ Chapter05/chapter5.py
@@ -137,6 +137,26 @@ class VanillaGNN(torch.nn.Module):
         h = self.gnn2(h, adjacency)
         return F.log_softmax(h, dim=1)
     
+    def fit(self, data, adjacency, epochs, learning_rate=0.01, weight_decay=5e-4):
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+        self.train()
+        for epoch in range(epochs+1):
+            optimizer.zero_grad()
+            out = self(data.x, adjacency)
+            loss = criterion(out[data.train_mask], data.y[data.train_mask])
+            acc = MLP.accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
+            loss.backward()
+            optimizer.step()
+
+            if epoch % 20 == 0:
+                val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
+                val_acc = MLP.accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
+                print(f'Epoch {epoch:>3} | Train Loss: {loss:.3f} | Train Acc:'
+                      f' {acc*100:>5.2f}% | Val Loss: {val_loss:.2f} | '
+                      f'Val Acc: {val_acc*100:.2f}%')
+                      
     @torch.no_grad()
     def test(self, data, adjacency):
         self.eval()
