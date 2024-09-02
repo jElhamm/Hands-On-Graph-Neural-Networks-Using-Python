395af2b5cc29d2646e7e9e9ec507de7a70a29601
jelhamm <elhamjafari200312@gmail.com>
2024-09-02 18:38:32 +0430

    Add Encoder class for VGAE model

--- Chapter10/chapter10.py
+++ Chapter10/chapter10.py
@@ -45,3 +45,17 @@ def set_seed(seed):
 
 set_seed(0)
     
+# -------------------------------- implements the encoder part of the Variational Graph Autoencoder (VGAE) model --------------------------------------
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super().__init__()
+        self.conv1 = GCNConv(dim_in, 2 * dim_out)
+        self.conv_mu = GCNConv(2 * dim_out, dim_out)
+        self.conv_logstd = GCNConv(2 * dim_out, dim_out)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
+    
