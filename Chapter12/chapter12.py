25b0811f9e080f3c5c1b3e41a16df9c340c14d09
jelhamm <elhamjafari200312@gmail.com>
2024-09-04 19:07:17 +0430

    Add custom GCNConv layer with message passing

--- Chapter12/chapter12.py
+++ Chapter12/chapter12.py
@@ -27,4 +27,26 @@ from sklearn.metrics import precision_recall_fscore_support
 from torch_geometric.nn import MessagePassing, GATConv, HANConv
 import warnings
 warnings.filterwarnings("ignore")
+
+
+# -------------------------------------- This class defines a `custom graph convolution layer` using message passing ----------------------------------------
+
+class GCNConv(MessagePassing):
+    def __init__(self, dim_in, dim_h):
+        super().__init__(aggr='add')
+        self.linear = nn.Linear(dim_in, dim_h, bias=False)
+
+    def forward(self, x, edge_index):
+        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        x = self.linear(x)
+        row, col = edge_index
+        deg = degree(col, x.size(0), dtype=x.dtype)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+        out = self.propagate(edge_index, x=x, norm=norm)
+        return out
+
+    def message(self, x, norm):
+        return norm.view(-1, 1) * x
     
